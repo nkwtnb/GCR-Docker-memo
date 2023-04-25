c1a4139cb3ce4ffcef1dbfe0078fc6123ade0a65
nkwtnb <n.watanabe0305@gmail.com>
2023-04-25 21:24:45 +0900

    add deploy shell

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+if [ $# -ne 4 ]; then
+  cat << EOS
+Please provide four arguments, including:
+  \$1: region (ex: us-west1)
+  \$2: project ID
+  \$3: repository of Artifact Registry
+  \$4: image
+EOS
+  exit 1
+fi
+# TODO 引数チェック
+docker build . -t $1-docker.pkg.dev/$2/$3/$4
+docker push $1-docker.pkg.dev/$2/$3/$4
+# TODO デプロイ用コマンド
